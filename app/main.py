33ab06efd46bdc08f0e8fd57ac91364d29b869b5
Mike <tradewcs@gmail.com>
2025-10-25 17:20:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    """
+    Caches function results based on arguments to avoid recomputation.
+    Works with hashable positional and keyword arguments.
+    """
+    cached = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> object:
+        key = (args, frozenset(kwargs.items()))
+        if key in cached:
+            print("Getting from cache")
+            return cached[key]
+
+        print("Calculating new result")
+        res = func(*args, **kwargs)
+        cached[key] = res
+        return res
+
+    return wrapper
